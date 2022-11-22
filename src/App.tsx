3baa8e707c067073221b97aa49eb85f15698c2f0
aryantiwari12 <aryantiwari1289@gmail.com>
2022-11-22 12:59:15 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Clock from './components/clock';
 
 function App() {
+  const [timerDays,setTimerDays]=useState();
+  const [timerHours,setTimerHours]=useState();
+  const [timerMinutes,setTimerMinutes]=useState();
+  const [timerSeconds,setTimerSeconds]=useState();
+  
+  let interval:any;
+  
+  const startTimer=()=>{
+    debugger
+    const countDownDate=new Date("Nov 23,2022").getTime();
+    
+    interval=setInterval(()=>{
+      const now=new Date().getTime();
+      
+      const distance=countDownDate-now;
+      console.log(distance)
+      
+      const days:any=Math.floor(distance/(24*60*60*1000))
+      const hours:any=Math.floor(distance%(24*60*60*1000)/(1000*60*60))
+      const Minutes:any=Math.floor(distance%(60*60*1000)/(1000*60))
+      const seconds:any=Math.floor(distance%(60*1000)/(1000))
+       
+     if(distance<0){
+        clearInterval(interval.current)
+     } else{
+      setTimerDays(days)
+      setTimerHours(hours)
+      setTimerMinutes(Minutes)
+      setTimerSeconds(seconds)
+     }
+      
+    })
+
+  };
+  useEffect(()=>{
+    startTimer()
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Clock
+        timerDays={timerDays}
+        timerHours={timerHours}
+        timerMinutes={timerMinutes}
+        timerSeconds={timerSeconds}
+       />
     </div>
   );
 }
